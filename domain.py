79cc9e437754607f17dc3c2f630ae7228c35a52c
TPan11 <pandetanay@gmail.com>
2016-05-10 00:22:40 -0400

    First Commit

---
+++ domain.py
@@ -0,0 +1,19 @@
+from urllib.parse import urlparse
+
+
+# Get sub domain name
+def get_sub_domain_name(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ''
+
+
+# Get domain name
+def get_domain_name(url):
+    try:
+        results = get_sub_domain_name(url).split('.')
+        return results[-2] + '.' + results[-1]
+    except:
+        return ''
+
